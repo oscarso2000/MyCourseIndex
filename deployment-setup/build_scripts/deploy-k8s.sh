95594e9ffe6396847d32e5113107c72c1a311890
Magd Bayoumi <mb2363@cornell.edu>
2020-03-17 04:47:39 +0000

    Validate = true for k8 deployment

--- deployment-setup/build_scripts/deploy-k8s.sh
+++ deployment-setup/build_scripts/deploy-k8s.sh
@@ -4,6 +4,17 @@ export PATH=$PATH:$(pwd)
 
 echo -e "${GREEN}==== Deploying RBAC role ====${NC}"
 cd deployment-setup/rbac/
-for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
+for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f; done
 echo -e "${GREEN}==== Done deploying RBAC role ====${NC}"
 echo ''
+
+# echo -e "${GREEN}==== Deploying iam role ====${NC}"
+# cd ../kube2iam/
+# for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
+# echo -e "${GREEN}==== Done deploying iam role ====${NC}"
+# echo ''
+# echo -e "${GREEN}==== Deploying external dns ====${NC}"
+# cd ../external_dns/
+# for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
+# echo -e "${GREEN}==== Done deploying external dns ====${NC}"
+# echo ''
