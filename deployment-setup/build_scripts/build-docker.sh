2f3f8271dacb0cfccbcb4f9fdc58a32cec279753
Ubuntu <ubuntu@ip-172-31-24-90.ec2.internal>
2020-03-17 00:46:51 +0000

    Attempting version number

--- deployment-setup/build_scripts/build-docker.sh
+++ deployment-setup/build_scripts/build-docker.sh
@@ -1,3 +1,5 @@
 docker build -t cs4300piazza .
 docker tag cs4300piazza mb2363/cs4300piazza:latest
+docker tag cs4300piazza mb2363/cs4300piazza:$TRAVIS_BUILD_NUMBER
 docker push mb2363/cs4300piazza:latest
+docker push mb2363/cs4300piazza:$TRAVIS_BUILD_NUMBER
