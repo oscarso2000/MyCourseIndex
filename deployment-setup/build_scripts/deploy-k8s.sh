fe895ee00f8abf0adf04c2b857a44d37e032261e
Magd Bayoumi <mb2363@cornell.edu>
2020-03-18 03:52:20 +0000

    Kube deployment

--- deployment-setup/build_scripts/deploy-k8s.sh
+++ deployment-setup/build_scripts/deploy-k8s.sh
@@ -4,11 +4,23 @@ export PATH=$PATH:$(pwd)
 
 echo "nameserver 172.31.36.87" | sudo tee -a /etc/resolv.conf
 
+# echo -e "${GREEN}==== Deploying RBAC role ====${NC}"
+# cd deployment-setup/rbac/
+# for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f; done
+# echo -e "${GREEN}==== Done deploying RBAC role ====${NC}"
+# echo ''
+
+docker build -t dummytest -f deployment-setup/dockerfile-kube-deploy
+docker run -it dummytest /bin/bash &
+export TEMPID=docker ps | grep dummytest | awk '{print $1;}'
+
 echo -e "${GREEN}==== Deploying RBAC role ====${NC}"
 cd deployment-setup/rbac/
-for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f; done
+for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do docker exec -it $TEMPID kubectl apply -f deployment-setup/rbac/$f; done
 echo -e "${GREEN}==== Done deploying RBAC role ====${NC}"
 echo ''
+
+# docker exec -it $TEMPID kubectl get nodes
 
 # echo -e "${GREEN}==== Deploying iam role ====${NC}"
 # cd ../kube2iam/
