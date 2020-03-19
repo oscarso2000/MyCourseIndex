ea829e571e6c4100af1eeb6c51749bf7542fa5b9
Magd Bayoumi <mb2363@cornell.edu>
2020-03-19 19:46:54 +0000

    Adding cluster Terraform service configuration for external dns

--- deployment-setup/build_scripts/deploy-k8s.sh
+++ deployment-setup/build_scripts/deploy-k8s.sh
@@ -25,7 +25,6 @@ then
 else
     failure=false
     echo -e "${GREEN}====             SUCCESS             ====${NC}"
-    echo -e "${GREEN}====     NAMESERVER: $dummyempty     ====${NC}"
 fi
 echo -e "${YELLOW}==== DONE TESTING NAMESERVER RESOLUTION ====${NC}"
 
