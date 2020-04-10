f9dc95f8d300b413c5c5ac76b424f15f5f0c4135
Magd Bayoumi <mb2363@cornell.edu>
2020-04-10 03:19:44 +0000

    Authorization

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,21 @@ import os
 from flask import Flask, render_template, request, redirect, flash
 #from db_setup import init_db, db_session
 from app.forms import SearchForm
+from flask_oidc import OpenIDConnect
+
+import os
 
 app = Flask(__name__)
+app.config.from_pyfile('/etc/cs4300-volume-cfg/cs4300app.cfg')
+# app.config.from_pyfile(os.path.join(os.path.join(os.getcwd(), "secrets"), "cs4300app.cfg"))
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 app.secret_key = 'insert AWS key'
 
+oidc = OpenIDConnect(app)
 
 @app.route('/', methods=['GET', 'POST'])
+@oidc.require_login
 def index():
     search = SearchForm(request.form)
     if request.method == 'POST':
