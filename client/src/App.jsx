ce79dff34fb70864babc2f8b605071d2f99be1aa
Magd Bayoumi <mb2363@cornell.edu>
2020-04-12 15:04:32 +0000

    feat(client): Added loading and autoRefresh features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
-import { WindMillLoading } from 'react-loadingg';
+import FadeIn from "react-fade-in";
+import Lottie from "react-lottie";
+import "bootstrap/dist/css/bootstrap.css";
 import Home from './containers/Home';
 import { About } from './components/About';
 import { Switch, Route } from 'react-router-dom';
 import './App.css'
 import axios from "axios";
 import { getToken } from './config/adalConfig';
+import ReactTimeout from 'react-timeout'
+
+import * as legoData from "./images/legoloading.json";
+import * as doneData from "./images/doneloading.json";
 
 // export const App: React.StatelessComponent = (): JSX.Element => {
 //     return (
@@ -18,6 +24,24 @@ import { getToken } from './config/adalConfig';
 //     )
 // }
 
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: legoData.default,
+    rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+    }
+}
+
+const defaultOptions2 = {
+    loop: false,
+    autoplay: true,
+    animationData: doneData.default,
+    rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+    }
+};
+
 const getAuth = (token) => {
     return axios.post(`https://www.mycourseindex.com/auth`, { "token": token }).then(
         (response) => {
@@ -28,29 +52,52 @@ const getAuth = (token) => {
     )
 }
 
-const divLoadingStyle = {
-    textAlign: 'center',
-    verticalAlign: 'middle'
-}
-
-const windmillStyle = {
-    display: 'inline-block'
-}
-
-export const App = () => {
+const App = (props) => {
 
     const [authorized, setAuthorized] = React.useState(false);
     const [loaded, setLoaded] = React.useState(false);
+    const [timedOut, setTimedOut] = React.useState(false);
+    const [done, setDone] = React.useState(false);
+    const [allowRefresh, setAllowRefresh] = React.useState(false)
 
-    const auth = getAuth(getToken());
-    auth.then((value) => {
-        setAuthorized(value);
-        setLoaded(true);
+    React.useEffect(() => {
+        setAllowRefresh(sessionStorage.getItem("allowRefresh") === "True");
+        if (allowRefresh) {
+            const auth = getAuth(getToken());
+            auth.then((value) => {
+                setAuthorized(value);
+                setLoaded(true);
+                setDone(true);
+                setTimedOut(true);
+            });
+        } else {
+            props.setTimeout(() => {
+                const auth = getAuth(getToken());
+                auth.then((value) => {
+                    setAuthorized(value);
+                    setLoaded(true);
+                    setDone(true);
+                    props.setTimeout(() => {
+                        setTimedOut(true);
+                        sessionStorage.setItem("allowRefresh", "True")
+                    }, 1000)
+                });
+            }, 3000);
+        }
+
+        // auth.then((value) => {
+        //     setAuthorized(value);
+        //     setLoaded(true);
+        //     props.setTimeout(() => {
+        //         setTimedOut(true);
+
+        //     }, 3000);
+        // });
     });
 
-    console.log("Auth");
-    console.log(auth);
-    if (loaded) {
+    console.log('done is ' + done + '\nloaded is ' + loaded + '\ntimedOut is ' + timedOut + '\nauthorized is ' + authorized);
+
+    if (loaded && done && timedOut && authorized) {
         if (authorized) {
             return (
                 <div>
@@ -69,15 +116,27 @@ export const App = () => {
                 </div>
             )
         }
+    } else if (allowRefresh) {
+        return (<div></div>)
     } else {
         return (
-            <div style={divLoadingStyle}>
-                <WindMillLoading color="#00CDCD" size="large" style={windmillStyle} />
-                Loading...
-            </div>
+            <div className="App">
+                <header className="App-header">
+                    <FadeIn>
+                        <div class="d-flex justify-content-center align-items-center">
+                            <h1>Fetching Authorization</h1>
+                            {done ?
+                                <Lottie options={defaultOptions2} height={480} width={480} />
+                                : (<Lottie options={defaultOptions} height={480} width={480} />)}
+                        </div>
+                    </FadeIn>
+                </header >
+            </div >
         )
     }
 }
+
+export const Application = ReactTimeout(App);
 
 // export const App = () => {
 //     if (true) {
