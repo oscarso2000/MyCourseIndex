46c8481b6f054ca723676ce0140f3505cb47275b
Magd Bayoumi <mb2363@cornell.edu>
2020-04-12 03:06:39 +0000

    feat(client): Loading animation before render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { WindMillLoading } from 'react-loadingg';
 import Home from './containers/Home';
 import { About } from './components/About';
 import { Switch, Route } from 'react-router-dom';
@@ -20,37 +21,59 @@ import { getToken } from './config/adalConfig';
 const getAuth = (token) => {
     return axios.post(`https://www.mycourseindex.com/auth`, { "token": token }).then(
         (response) => {
-            console.log(response.data);
-            console.log(typeof response.data)
+            // console.log(response.data);
+            // console.log(typeof response.data)
             return (response.data === "OK")
         }
     )
 }
 
+const divLoadingStyle = {
+    textAlign: 'center',
+    verticalAlign: 'middle'
+}
+
+const windmillStyle = {
+    display: 'inline-block'
+}
+
 export const App = () => {
 
     const [authorized, setAuthorized] = React.useState(false);
+    const [loaded, setLoaded] = React.useState(false);
 
     const auth = getAuth(getToken());
-    auth.then((value) => setAuthorized(value));
+    auth.then((value) => {
+        setAuthorized(value);
+        setLoaded(true);
+    });
 
     console.log("Auth");
     console.log(auth);
-    if (authorized) {
-        return (
-            <div>
-                <Switch>
-                    <Route exact={true} path="/" component={Home} />
-                    <Route path="/about" component={About} />
-                </Switch>
-            </div>
-        )
+    if (loaded) {
+        if (authorized) {
+            return (
+                <div>
+                    <Switch>
+                        <Route exact={true} path="/" component={Home} />
+                        <Route path="/about" component={About} />
+                    </Switch>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <p>
+                        ERROR!
+                    </p>
+                </div>
+            )
+        }
     } else {
         return (
-            <div>
-                <p>
-                    ERROR!
-                </p>
+            <div style={divLoadingStyle}>
+                <WindMillLoading color="#00CDCD" size="large" style={windmillStyle} />
+                Loading...
             </div>
         )
     }
