afda17e600311b4e314bcfbc365ca095ed73c5a3
Oscar <os2304@columbia.edu>
2020-04-12 23:48:55 -0400

    about

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -11,16 +11,13 @@ export const About: React.StatelessComponent = (): JSX.Element => (
         <div className="center">
             <h4>About</h4>
             <h4><a href="https://github.com/oscarso2000/mycourseindex">Source code on Github</a></h4>
-            <p>
-                The authors of the mycourseindex project are: Magd Bayoumi (mb2363), Jenna Kressin (jek343), Souleiman Benhida (sb2342), Sheetal Athrey (spa42), Oscar So (ons4) 
-                MyCourseIndex is...  
-            </p>  
-            <ul>
-                Attributions:
-                <li> Icons: <a href="https://www.flaticon.com/free-icon/magnifying-glass-browser_70490#term=search&page=1&position=45">Magnifying Glass</a></li>
-                    <li><a href="https://www.flaticon.com/free-icon/text-document_32329#term=text&page=1&position=9">Outline</a></li>
-                    <li><a href="https://codepen.io/rbv912/pen/dYbqLQ">Loading Animation</a></li>
-            </ul>
+            <p> The authors of the mycourseindex project are: </p>
+            <a href="https://github.com/bayoumi17m">Magd Bayoumi (mb2363)</a>
+            <a href="https://github.com/jek343">Jenna Kressin (jek343)</a>
+            <a href="https://github.com/soule">Souleiman Benhida (sb2342)</a>
+            <a href="https://github.com/sheetal-athrey">Sheetal Athrey (spa42)</a>
+            <a href="https://github.com/oscarso2000">Oscar So (ons4) </a>
+            <p>MyCourseIndex is...</p>  
         </div>
     </div>
 )
