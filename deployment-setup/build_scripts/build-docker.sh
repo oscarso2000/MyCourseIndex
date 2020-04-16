0e5c8f7e025acd65091e6eca9d6a040f2d87a54c
Magd Bayoumi <mb2363@cornell.edu>
2020-04-16 01:50:13 +0000

    docs: Updating docs and updating build pipeline to render docs online

--- deployment-setup/build_scripts/build-docker.sh
+++ deployment-setup/build_scripts/build-docker.sh
@@ -3,3 +3,12 @@ docker tag cs4300piazza oscarso2000/cs4300piazza:latest
 docker tag cs4300piazza oscarso2000/cs4300piazza:$TRAVIS_BUILD_NUMBER
 docker push oscarso2000/cs4300piazza:latest
 docker push oscarso2000/cs4300piazza:$TRAVIS_BUILD_NUMBER
+
+cd docs
+docker build -t cs4300docs -f docs/Dockerfile .
+docker tag cs4300docs oscarso2000/cs4300docs:latest
+docker tag cs4300docs oscarso2000/cs4300docs:$TRAVIS_BUILD_NUMBER
+docker push oscarso2000/cs4300docs:latest
+docker push oscarso2000/cs4300docs:$TRAVIS_BUILD_NUMBER
+
+cd ..
