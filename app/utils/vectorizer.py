104e369553d2ce9f3d4cc52da882396d8801b761
Oscar <os2304@columbia.edu>
2020-04-18 20:32:11 -0400

    courseVecDictionary

---
+++ app/utils/vectorizer.py
@@ -0,0 +1,24 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from app.utils import tokenize
+
+tokenizer = tokenize.tokenized_already
+
+# S3 JSON Format:
+# { "CS4300": {"Piazza": {PostID: content, PostID2: content2}, "Textbook": {DocId: content, DocID2: content2} } }
+
+fromS3 = loadJSONFILE from S3 !!!
+
+docVecDictionary = {}
+
+for course in fromS3:
+    vec = TfidfVectorizer(tokenizer = tokenizer)
+    documents = []
+    for source in course.value:
+        for content in source.value:
+            unique = [source.key, content.key]
+            unique.extend(content.value)
+            #looks like ["Piazza", "671", "this", "is", "the", "post"]      
+            documents.append(unique)
+    docVecDictionary[course.key] = vec.fit_transform(documents).toarray()
+    
+#docVecDictionary is full dictionary of all documents in all courses. Should be a global variable.
