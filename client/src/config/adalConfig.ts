d659148f53afb94f784315cb0a2fe73b85ec7ff3
bayoumi17m <mb2363@cornell.edu>
2020-04-20 12:10:21 -0400

    Update adalConfig.ts

--- client/src/config/adalConfig.ts
+++ client/src/config/adalConfig.ts
@@ -6,8 +6,8 @@ const clientId = '41e533e7-b473-4087-86a1-c00f86b39487';
 const adalConfig: AdalConfig = {
     tenant: tenantSubdomain + '.onmicrosoft.com',
     clientId: '41e533e7-b473-4087-86a1-c00f86b39487',
-    // redirectUri: 'https://www.mycourseindex.com/oidc/callback',
-    redirectUri: 'http://localhost:3000/oidc/callback',
+    redirectUri: 'https://www.mycourseindex.com/oidc/callback',
+    // redirectUri: 'http://localhost:3000/oidc/callback',
     endpoints: {
         api: 'https://' + tenantSubdomain + '.onmicrosoft.com/' + clientId,
     },
