c53f033aef218c74d969d5e49f2b85b5a16c3883
Jenna <jek343@cornell.edu>
2020-04-20 17:59:43 -0700

    vectorized queries

--- preprocess/preprocessquery.py
+++ preprocess/preprocessquery.py
@@ -1,9 +1,10 @@
 import preprocessutils
 import re
 import numpy as np
+import pickle
 
 tokenize_method = preprocessutils.tokenize_SpaCy
-query = "-'alice bleh' loves the +'sun' '10/8/2020 home '^3 homework +'hello' 'sunshine'^2 'sunshine'^3"
+query = "-'gain' +'cumulative' 'score'^3"
 
 def get_pos(query):
     pos = re.findall("\+'(.*?)'",query)
@@ -22,7 +23,7 @@ def get_mult(query):
     words = list(map(lambda t: re.findall("'([^']+)'\^", t)[0], mult))
     #if two words are the exact same, uses last exponent
     m = dict(zip(words, exps))
-    return words, m
+    return list(m.keys()), m
 
 def remove(query):
     #remove +/-
@@ -43,3 +44,29 @@ neg = get_neg(query)
 mult, m = get_mult(query)
 query = remove(query)
 all_tokens = get_all_tokens(query, pos, neg, mult)
+
+z = pickle.load(open("piazza_data_preprocessed.p","rb" ))
+
+def create_matrix(tokens, docs):
+    len_docs = 0
+    for t in z:
+        len_docs += len(z[t])
+    len_toks = len(tokens)
+    mat = np.zeros((len_toks, len_docs))
+    doc_ids = []
+    for t, post_ids in z.items():
+        doc_ids += post_ids
+        for num_tok, tok in enumerate(tokens):
+            for num_post, (post_id, counter) in enumerate(post_ids.items()):
+                if tok in counter:
+                    mat[num_tok, num_post] = counter[tok]
+    return mat, tokens, doc_ids
+
+            
+#matrix, row_ids, col_ids
+mat, tokens, doc_ids = create_matrix(all_tokens, z)
+pos_mat, pos_tokens, doc_ids = create_matrix(pos, z)
+neg_mat, neg_tokens, doc_ids = create_matrix(neg, z)
+mult_mat, mult_tokens, doc_ids = create_matrix(mult, z)
+#tokens is in order of rows in matrix
+#doc_ids is in order of columns in matrix
