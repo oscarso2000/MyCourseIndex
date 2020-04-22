25a15f74ae98cacfe77f02caae27e5339e98cec5
Oscar <os2304@columbia.edu>
2020-04-22 00:22:16 -0400

    LSI_SVD not tested, code should run by logic for now will test in future

--- app/search/similarity.py
+++ app/search/similarity.py
@@ -46,6 +46,31 @@ def cosineSimSplit(query, courseVecDictionary, course):
     
     return piazza_sim, other_sim
     
+def LSI_SVD(query, courseVecDictionary, course):
+    #courseVecDictionary[class selected]
+    vec, docVectorizerArray = courseVecDictionary[course]
+    
+    query = utils.tokenize_SpaCy(query)
+    queryVectorizerArray = np.zeroes((docVectorizerArray.shape[1],))
+    feature_list = vec.get_feature_names()
+
+    for w in query:
+        idx = feature_list.index(w)
+        queryVectorizerArray[idx] += 1.0
+        
+    #using k = 2 rn, need to develop code to see what is optimal k
+    k = 2 
+    u,s,v_t = np.linalg.svd(docVectorizerArray) #svd on tfidf documents
+    q = queryVectorizerArray
+    q_hat = np.matmul(np.transpose(u[:,:k]),q)
+    
+    sim = []
+    for i in range(docVectorizerArray.shape[0]):
+        num = np.matmul(np.matmul(np.diag(s[:k]),v_t[:k,i]),np.transpose(q_hat))
+        denom = np.linalg.norm(np.matmul(np.diag(s[:k]),v_t[:k,i]))*np.linalg.norm(q_hat)
+        sim.append(num/denom)
+
+    return np.array(sim)
     
     
 #for local use
