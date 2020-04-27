bcf44963e645a43b3155f129aa6c8cae721cf13a
Jenna <jek343@cornell.edu>
2020-04-27 12:50:42 -0700

    one method to call

--- preprocess/preprocessquery.py
+++ preprocess/preprocessquery.py
@@ -4,7 +4,6 @@ import numpy as np
 import pickle
 
 tokenize_method = preprocessutils.tokenize_SpaCy
-query = "+'cumulative' +'system discount' -'football' 'score'^3 'unprecidented'^3 'scores'^4 'score'^2"
 
 flatten = lambda l: [item for sublist in l for item in sublist]
 
@@ -49,37 +48,20 @@ def get_all_tokens(query, pos, neg, mult):
     all_tokens = sorted(list(filter(regex.match, all_tokens)))
     return all_tokens
 
-pos = get_pos(query)
-neg = get_neg(query)
-mult, m = get_mult(query)
-query = remove(query)
-all_tokens = get_all_tokens(query, pos, neg, mult)
-
-z = pickle.load(open("piazza_data_preprocessed.p","rb" ))
-
 def create_matrix(tokens, docs):
     len_docs = 0
-    for t in z:
-        len_docs += len(z[t])
+    for t in docs:
+        len_docs += len(docs[t])
     len_toks = len(tokens)
     mat = np.zeros((len_toks, len_docs))
     doc_ids = []
-    for t, post_ids in z.items():
+    for t, post_ids in docs.items():
         doc_ids += post_ids
         for num_tok, tok in enumerate(tokens):
             for num_post, (post_id, counter) in enumerate(post_ids.items()):
                 if tok in counter:
                     mat[num_tok, num_post] = counter[tok]
     return mat, tokens, doc_ids
-
-            
-#matrix, row_ids, col_ids
-mat, tokens, doc_ids = create_matrix(all_tokens, z)
-pos_mat, pos_tokens, doc_ids = create_matrix(pos, z)
-neg_mat, neg_tokens, doc_ids = create_matrix(neg, z)
-mult_mat, mult_tokens, doc_ids = create_matrix(mult, z)
-#tokens is in order of rows in matrix
-#doc_ids is in order of columns in matrix
 
 def bool_vec(pos_mat, neg_mat, mult_tokens, mult_mat, m):
     pos_bool = np.prod(pos_mat, axis=0)
@@ -95,8 +77,26 @@ def bool_vec(pos_mat, neg_mat, mult_tokens, mult_mat, m):
 
     return pos_idx * neg_idx * mult_idx
 
-print(bool_vec(pos_mat, neg_mat, mult_tokens, mult_mat, m))
 
 
+def run(query):
+    pos = get_pos(query)
+    neg = get_neg(query)
+    mult, m = get_mult(query)
+    query = remove(query)
+    all_tokens = get_all_tokens(query, pos, neg, mult)
 
+    z = pickle.load(open("piazza_data_preprocessed.p","rb" ))
+
+    #matrix, row_ids, col_ids
+    mat, tokens, doc_ids = create_matrix(all_tokens, z)
+    pos_mat, pos_tokens, doc_ids = create_matrix(pos, z)
+    neg_mat, neg_tokens, doc_ids = create_matrix(neg, z)
+    mult_mat, mult_tokens, doc_ids = create_matrix(mult, z)
+    #tokens is in order of rows in matrix
+    #doc_ids is in order of columns in matrix
+    print(bool_vec(pos_mat, neg_mat, mult_tokens, mult_mat, m))
+
+query = "+'cumulative' +'system discount' -'football' 'score'^3 'unprecidented'^3 'scores'^4 'score'^2"
+run(query)
 
