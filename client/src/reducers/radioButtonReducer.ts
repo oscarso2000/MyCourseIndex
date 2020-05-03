a9354beb27254ae09e216c4b328c4bad38701d79
Oscar So <oscarso2000jobs@gmail.com>
2020-05-03 02:26:56 -0400

    Update radioButtonReducer.ts

--- client/src/reducers/radioButtonReducer.ts
+++ client/src/reducers/radioButtonReducer.ts
@@ -3,7 +3,6 @@ import { IAction } from '.';
 export const radioButtonReducer = (state = 'Default', action: IAction) => {
     switch (action.type) {
         case 'SET_SEARCH':
-          console.log(action.payload);
           return action.payload;
         default:
           return state;
