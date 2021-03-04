6bce418c056dc1c63cf261380fa38cabf6228a3a
Oscar <os2304@columbia.edu>
2021-03-03 20:20:01 -0500

    new members updated Abouts page

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -6,12 +6,22 @@ import Oscar from '../images/Oscar_So.jpg';
 import Sheetal from '../images/Sheetal_A.jpg';
 import Jenna from '../images/Jenna_Kressin.jpeg';
 import Souleiman from '../images/Souleiman_B.png';
-import David from '../images/David_Kim.jpg';
-import Julia from '../images/Julia_Ng.jpg';
+import David from '../images/David_K.png';
+import Julia_N from '../images/Julia_Ng.jpg';
 import Katie from '../images/Katie_Yang.jpg';
 import Jessie from '../images/Jessie_Lee.png';
-import John from '../images/John_O.jpg'
-import Elva from '../images/Elva_Gao.jpg'
+import John from '../images/John_O.jpg';
+import Elva from '../images/Elva_Gao.jpg';
+import Julia_A from '../images/Julia_A.jpg';
+import Melinda from '../images/Melinda_F.jpg';
+import Ruchika from '../images/Ruchika_D.png';
+import Vaishnavi from '../images/Vaishnavi_G.jpeg';
+import Felix from '../images/Felix_H.jpg';
+import Divya from '../images/Divya_D.jpg';
+import Chelsea from '../images/Chelsea_X.jpeg';
+import Edward from '../images/Edward_G.jpg';
+import Tobi from '../images/Tobi_A.png';
+
 
 import '../style/About.css';
 
@@ -24,7 +34,7 @@ export const About: React.StatelessComponent = (): JSX.Element => (
         <div className="center">
             <h4>About</h4>
 
-            <p>MyCourseIndex was initially a project started for CS/INFO 4300: Language and Information that acts as an essential search engine for Cornell students and their courses. This search gathers all information from Piazza posts to Textbook and Notes Resources and returns valid results for the student to utilize. It is now being maintained and improved by members of <a href="https://cornelldata.science">Cornell Data Science Project Team</a>.</p>
+            <p>MyCourseIndex was initially a project started for CS/INFO 4300: Language and Information that acts as an essential search engine for Cornell students and their courses. This search gathers all information from Piazza posts to Textbook and Notes Resources and returns valid results for the student to utilize. It is now being maintained and improved by members of <b><a href="https://cornelldata.science">Cornell Data Science Project Team</a></b>.</p>
 
             <div className="centertext">
                 <p> <b>Co-Founders:</b> </p>
@@ -56,7 +66,6 @@ export const About: React.StatelessComponent = (): JSX.Element => (
             <div className="centertext">
                 <p> <b>Team:</b> </p>
             </div>
-
             <div className="innerimage">
                 <div className="item">
                     <img src={David} />
@@ -67,12 +76,9 @@ export const About: React.StatelessComponent = (): JSX.Element => (
                     <span className="caption"><a href="https://github.com/shljessie">Jessie Lee (sl994)</a></span>
                 </div>
                 <div className="item">
-                    <img src={Julia} />
+                    <img src={Julia_N} />
                     <span className="caption"><a href="https://github.com/ngjulia">Julia Ng (jen67)</a></span>
                 </div>
-            </div>
-            <p></p>
-            <div className="innerimage">
                 <div className="item">
                     <img src={Katie} />
                     <span className="caption"><a href="https://github.com/yangkt">Katie Yang (ky289)</a></span>
@@ -83,22 +89,72 @@ export const About: React.StatelessComponent = (): JSX.Element => (
                     {/* <span className="caption"><a href="https://github.com/elvagao66">Elva Gao (yg357)</a></span> */}
                     <span className="caption"><a href="https://github.com/elvaaaa">Elva Gao (yg357)</a></span>
                 </div>
+            </div>
+            <p></p>
+            <div className="innerimage">
+                <div className="item">
+                    <img src={Ruchika} />
+                    <span className="caption"><a href="https://github.com/rdongre7">Ruchika Dongre (rd438)</a></span>
+                </div>
+                <div className="item">
+                    <img src={Julia_A} />
+                    <span className="caption"><a href="https://github.com/julia-allen">Julia Allen (jra264)</a></span>
+                </div>
+                <div className="item">
+                    <img src={Melinda} />
+                    <span className="caption"><a href="https://github.com/MelindaFang-code">Melinda Fang (cf348)</a></span>
+                </div>
+                <div className="item">
+                    <img src={Divya} />
+                    <span className="caption"><a href="https://github.com/Divya0204">Divya Damodaran (dd492)</a></span>
+                </div>
+                <div className="item">
+                    <img src={Vaishnavi} />
+                    <span className="caption"><a href="https://github.com/vaishnavi17">Vaishnavi Gupta (vg222)</a></span>
+                </div>
+            </div>
+            <p></p>
+            <div className="innerimage">
+                <div className="item">
+                    <img src={Felix} />
+                    <span className="caption"><a href="https://github.com/FelixHohne">Felix Hohne (fmh42)</a></span>
+                </div>
+                <div className="item">
+                    <img src={Tobi} />
+                    <span className="caption"><a href="https://github.com/AbioticFactor">Tobi Alade (aoa34)</a></span>
+                </div>
+                <div className="item">
+                    <img src={Chelsea} />
+                    <span className="caption"><a href="https://github.com/6390wer">Chelsea Xiong (qx27)</a></span>
+                </div>
+                <div className="item">
+                    <img src={Edward} />
+                    <span className="caption"><a href="https://github.com/xegux">Edward Gu (elg227)</a></span>
+                </div>
+            </div>
+            <p></p>
+
+            <div className="centertext">
+                <p> <b>Previous Members:</b> </p>
+            </div>
+            <div className="innerimage">
                 <div className="item">
                     <img src={John} />
                     <span className="caption"><a href="https://github.com/jodonnell77">John O'Donnell (jro79)</a></span>
                 </div>
             </div>
-
             <p></p>
-            <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
-                <a href="https://v1.mycourseindex.com" target="_blank" rel="noopener noreferrer">
-                    <h3 className="home-1">MyCourseIndex</h3>
-                    <h3 className="home-2">Search V1</h3>
+
+
+            <div className="page_links" style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
+                <a href="Terms" target="_blank" rel="noopener noreferrer">
+                    <h3 className="home-1">Terms &amp;</h3>
+                    <h3 className="home-2">Conditions</h3>
                 </a>
 
-                <a href="https://v2.mycourseindex.com" target="_blank" rel="noopener noreferrer">
-                    <h3 className="home-1">MyCourseIndex</h3>
-                    <h3 className="home-2">Search V2</h3>
+                <a href="Policy" target="_blank" rel="noopener noreferrer">
+                    <h3 className="home-1">Privacy</h3>
+                    <h3 className="home-2">Policy</h3>
                 </a>
 
                 <a href="https://github.com/oscarso2000/mycourseindex" target="_blank" rel="noopener noreferrer">
@@ -106,7 +162,6 @@ export const About: React.StatelessComponent = (): JSX.Element => (
                     <h3 className="home-2">Source Code</h3>
                 </a>
             </div>
-
         </div>
     </div>
 )
